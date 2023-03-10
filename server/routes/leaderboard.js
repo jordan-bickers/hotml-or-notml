02e3fea39997ded5f559c1adc7b3e7e1ad782b4d
jordan-bickers <76583443+jordan-bickers@users.noreply.github.com>
2023-03-10 13:59:04 +1300

    countVotes function, but not displaying

--- server/routes/leaderboard.js
+++ server/routes/leaderboard.js
@@ -7,9 +7,25 @@ const router = express.Router()
 router.get('/', (req, res) => {
   db.getTags()
     .then((tags) => {
-      console.log(tags)
+      // console.log(tags)
       res.render('index', { tags: tags })
     })
+    .catch((err) => {
+      res.status(500).send('DATABASE ERROR: ' + err.message)
+    })
+})
+
+router.post('/vote', (req, res) => {
+  const id = Number(req.body.id)
+  db.createVote(id)
+    .then(() => {
+      db.countVotes()
+    })
+    .then((data) => {
+      console.log(data)
+      res.redirect('/')
+    })
+
     .catch((err) => {
       res.status(500).send('DATABASE ERROR: ' + err.message)
     })
