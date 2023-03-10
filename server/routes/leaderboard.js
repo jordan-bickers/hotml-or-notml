5a8f849a9a985d9c4db2f049b73da857a0d292bb
Louis Dyer <111328948+doilyuser@users.noreply.github.com>
2023-03-10 14:56:55 +1300

    profile route is pulling the right object

--- server/routes/leaderboard.js
+++ server/routes/leaderboard.js
@@ -39,6 +39,23 @@ router.get('/', (req, res) => {
     })
 })
 
+router.get('/profile/:id', (req, res) => {
+  const id = Number(req.params.id)
+  db.countVotes()
+    .then((tags) => {
+      console.log(tags)
+      const profileData = tags.find((el) => {
+        return el.id === id
+      })
+      console.log(profileData)
+
+      res.render('profile', { tags: profileData })
+    })
+    .catch((err) => {
+      res.status(500).send('DATABASE ERROR: ' + err.message)
+    })
+})
+
 router.post('/vote', (req, res) => {
   res.redirect('/')
 })
